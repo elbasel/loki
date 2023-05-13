6c5350161ac6ee48ba8abfd8ad6cebee503c9aed
elbasel <abdelrahman.elbasel@gmail.com>
2023-05-13 10:08:42 +0300

    Minor style changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,21 @@
 import Link from "next/link";
 
+const linkStyles =
+  "bg-black border-blue-700 hover:bg-white hover:text-black transition-all text-3xl w-fit px-4 py-2 mx-auto rounded-lg";
+
 export default function Home() {
   return (
     <main>
       <h1>Home</h1>
-      <ul className="flex gap-2">
-        <Link className="navLink" href="/ask">
-          Ask
+      <ul className="flex flex-col gap-2">
+        <Link className={linkStyles} href="/open-ai">
+          Chat with an artificial intelligence
         </Link>
-        <Link className="navLink" href="/google">
-          Google
+        <Link className={linkStyles} href="/google">
+          Search Google easily
         </Link>
-        <Link className="navLink" href="/open-ai">
-          Open Ai
-        </Link>
+
+        <p className={linkStyles}>Automate your life (coming soon)</p>
       </ul>
     </main>
   );
