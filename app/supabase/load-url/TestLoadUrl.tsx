bc8bd696778f4d8356da4194d02ab61e10faeaec
elbasel <abdelrahman.elbasel@gmail.com>
2023-05-23 02:28:47 +0300

    Another refactor

--- app/supabase/load-url/TestLoadUrl.tsx
+++ app/supabase/load-url/TestLoadUrl.tsx
@@ -1,37 +1,46 @@
 "use client";
 
 import { Button, InputWithRef, Output } from "@app/UI";
-import { type LokiDoc, loadUrl } from "@app/open-ai/actions";
-import { useRef, useState } from "react";
+import { type UrlInfo as UrlInfo, loadUrl } from "@app/open-ai/actions";
+import { useEffect, useRef, useState } from "react";
 
-type relevantInfo = string[];
-type ClientStatus = "idle" | "loading" | "success" | "error" | string;
+type _ClientStatus = "idle" | "loading" | "success" | "warning" | "error";
 
 export const TestLoadUrl: React.FC = () => {
   const inputRef = useRef<any>();
-  //   const [relevantInfo, setRelevantInfo] = useState<relevantInfo>([])
-  const [serverResponse, setServerResponse] = useState<any>(null);
-  const [clientStatus, setClientStatus] = useState<ClientStatus>("idle");
+  const [serverResponse, setServerResponse] = useState<UrlInfo[]>();
+  const [clientStatus, setClientStatus] = useState<_ClientStatus>("idle");
+  const [urlToLoad, setUrlToLoad] = useState<string>("");
+  const [urlInfo, setUrlInfo] = useState<UrlInfo>();
 
-  const getServerResponse = async () => {
-    const urlToLoad = inputRef.current?.value;
-    if (!urlToLoad)
-      return setClientStatus("error: the value of the input is == null");
-    setClientStatus("loading");
-    const serverResponse: LokiDoc[] = await loadUrl(urlToLoad);
-    const { url, title, html }: LokiDoc = serverResponse[0];
-    setServerResponse(serverResponse);
-    setClientStatus("success");
+  const getUrlInfo = async (urlToLoad: string) => {
+    const serverResponse: UrlInfo[] = await loadUrl(urlToLoad);
+    const doc: UrlInfo = serverResponse[0];
+
+    return doc;
   };
+
+  const handleUrlChange = () => {
+
+   if (!urlToLoad) return setClientStatus("idle");
+    setClientStatus("loading");
+    const urlInfo: UrlInfo = getUrlInfo(urlToLoad);
+
+   
+  };
+  useEffect(() => {}, [urlToLoad]);
 
   return (
     <main>
       <h1>Test Load Url</h1>
-      <form>
-        <InputWithRef ref={inputRef} />
-        <Button type="submit">Load URL into Long term ai memory</Button>
-        <Output></Output>
-      </form>
+      <InputWithRef ref={inputRef} />
+      <Button
+        onClick={() => setUrlToLoad(inputRef.current?.value)}
+        type="submit"
+      >
+        Load URL into Long term ai memory
+      </Button>
+      <Output></Output>
     </main>
   );
 };
