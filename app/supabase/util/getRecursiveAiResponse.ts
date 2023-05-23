0beae7c4371fb53d90f80e2aa9fd653f70054db5
elbasel <abdelrahman.elbasel@gmail.com>
2023-05-23 23:14:12 +0300

    Probably working

--- app/supabase/util/getRecursiveAiResponse.ts
+++ app/supabase/util/getRecursiveAiResponse.ts
@@ -13,8 +13,15 @@ export const getRecursiveAiResponse = async (
   const aiResponse = await getChatCompletionFromText(
     `use '${context}' to answer '${query}', format your answer as a reply in a chat. if you need more information to answer the query, please respond only with 'no-info' and nothing else!`
   );
+  const allSupaBaseDocs = await getAllSupabaseDocs();
+  const aiResponseWithAllDocs = await getChatCompletionFromText(
+    `use '${allSupaBaseDocs.join(
+      " "
+    )}' to answer '${query}', format your answer as a reply in a chat. if you need more information to answer the query, please respond only with 'no-info' and nothing else!`
+  );
 
-  return aiResponse;
+  console.log({ context, aiResponseWithAllDocs, aiResponse });
+  return aiResponseWithAllDocs;
   console.log({ aiResponse });
   if (currentDepth === 2 || currentDepth > _MAX_RECURSION_DEPTH) {
     return aiResponse;
